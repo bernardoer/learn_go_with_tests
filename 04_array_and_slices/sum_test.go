027a4fe7eaa49d935200b6909c424769395ae38e
bernardoer <mr.bernardobvb@gmail.com>
2026-08-17 16:27:08 -0300

    SumAll strategy

--- 04_array_and_slices/sum_test.go
+++ 04_array_and_slices/sum_test.go
@@ -1,13 +1,39 @@
 package main
 
-import "testing"
+import (
+	"slices"
+	"testing"
+)
 
 func TestSum(t *testing.T) {
-	numbers := [5]int{1, 2, 3, 4, 5}
-	got := Sum(numbers)
-	want := 15
+	t.Run("Collection of 5 numbers", func(t *testing.T) {
 
-	if got != want {
-		t.Errorf("got %d want %d given, %v", got, want, numbers)
+		numbers := []int{1, 2, 3, 4, 5}
+		got := Sum(numbers)
+		want := 15
+		if got != want {
+			t.Errorf("got %d want %d given, %v", got, want, numbers)
+		}
+	})
+
+	t.Run("collection of any size", func(t *testing.T) {
+		numbers := []int{1, 2, 3}
+
+		got := Sum(numbers)
+		want := 6
+
+		if got != want {
+			t.Errorf("got %d want %d given, %v", got, want, numbers)
+		}
+	})
+}
+
+func TestSumAll(t *testing.T) {
+
+	got := SumAll([]int{1, 2}, []int{0, 9})
+	want := []int{3, 9}
+
+	if !slices.Equal(got, want) {
+		t.Errorf("got %v want %v", got, want)
 	}
 }
