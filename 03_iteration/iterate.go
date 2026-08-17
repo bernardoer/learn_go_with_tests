fa7aa716c58e788e403114f9945206db07778d75
bernardoer <mr.bernardobvb@gmail.com>
2026-08-17 11:28:24 -0300

    finish the iterate section with the exercises

--- 03_iteration/iterate.go
+++ 03_iteration/iterate.go
@@ -1,11 +1,15 @@
 package iteration
 
-const repeatCount = 5
+import "strings"
 
-func Repeat(character string) string {
-	var repeated string
-	for i := 0; i < repeatCount; i++ {
-		repeated += character
-	}
-	return repeated
+func Repeat(character string, repeatCount int) string {
+	// Function create on class->
+
+	// var repeated strings.Builder
+	// for i := 0; i < repeatCount; i++ {
+	// 	repeated.WriteString(character)
+	// }
+
+	// Use String library function to do the same job
+	return strings.Repeat(character, repeatCount)
 }
