02f6a789d06db4f8e3372bed12d6b7ab788e2580
bernardoer <mr.bernardobvb@gmail.com>
2026-08-18 16:38:18 -0300

    looking into variables addresses

--- 05_structs_methods_interfaces/shapes.go
+++ 05_structs_methods_interfaces/shapes.go
@@ -7,12 +7,12 @@ type Shape interface {
 }
 
 type Rectangle struct {
-	width  float64
-	height float64
+	Width  float64
+	Height float64
 }
 
 func (r Rectangle) Area() float64 {
-	return r.width * r.height
+	return r.Width * r.Height
 }
 
 type Circle struct {
@@ -23,10 +23,19 @@ func (c Circle) Area() float64 {
 	return math.Pi * c.Radius * c.Radius
 }
 
+type Triangle struct {
+	Base   float64
+	Height float64
+}
+
+func (t Triangle) Area() float64 {
+	return t.Base * t.Height * 0.5
+}
+
 func Perimeter(rectangle Rectangle) float64 {
-	return 2 * (rectangle.width + rectangle.height)
+	return 2 * (rectangle.Width + rectangle.Height)
 }
 
 func Area(rectangle Rectangle) float64 {
-	return rectangle.width * rectangle.height
+	return rectangle.Width * rectangle.Height
 }
