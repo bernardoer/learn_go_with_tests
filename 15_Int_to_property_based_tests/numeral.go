f092f442c4f9a21c6ed06a51f681d100fd864c9f
bernardoer <mr.bernardobvb@gmail.com>
2026-08-26 11:07:01 -0300

    initial test structure for the conversion

--- 15_Int_to_property_based_tests/numeral.go
+++ 15_Int_to_property_based_tests/numeral.go
@@ -1,8 +1,24 @@
 package inttopropertybasedtests
 
+import "strings"
+
 func ConvertToRoman(arabic int) string {
-	if arabic == 2 {
-		return "II"
+
+	var result strings.Builder
+
+	for arabic > 0 {
+		switch {
+		case arabic > 4:
+			result.WriteString("V")
+			arabic -= 5
+		case arabic > 3:
+			result.WriteString("IV")
+			arabic -= 4
+		default:
+			result.WriteString("I")
+			arabic--
+		}
 	}
-	return "I"
+
+	return result.String()
 }
