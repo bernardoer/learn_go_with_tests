b02f096a35db0fa05a4194c71f7d67d499ef191f
bernardoer <mr.bernardobvb@gmail.com>
2026-09-11 17:07:00 -0300

    finish reading files

--- 17_Reading_files/blogposts/blogposts.go
+++ 17_Reading_files/blogposts/blogposts.go
@@ -1,10 +1,30 @@
 package blogposts
 
-import "testing/fstest"
+import (
+	"io/fs"
+)
 
-type Post struct {
+func NewPostsFromFS(fileSystem fs.FS) ([]Post, error) {
+	dir, err := fs.ReadDir(fileSystem, ".")
+	if err != nil {
+		return nil, err
+	}
+	var posts []Post
+	for _, f := range dir {
+		post, err := getPost(fileSystem, f.Name())
+		if err != nil {
+			return nil, err
+		}
+		posts = append(posts, post)
+	}
+	return posts, nil
 }
 
-func NewPostsFromFS(fileSystem fstest.MapFS) []Post {
-	return nil
+func getPost(fileSystem fs.FS, fileName string) (Post, error) {
+	postFile, err := fileSystem.Open(fileName)
+	if err != nil {
+		return Post{}, err
+	}
+	defer postFile.Close()
+	return newPost(postFile)
 }
--- 17_Reading_files/blogposts/go.mod
+++ 17_Reading_files/blogposts/go.mod
@@ -1,3 +1,3 @@
-module github.com/bernardoer/blogposts
+module github.com/bernardoer/learn_go_with_tests/17_Reading_files/blogposts
 
 go 1.26.6
