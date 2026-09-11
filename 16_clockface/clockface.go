6e7d14e6037569f6cb775ab0ea4734679af33e84
bernardoer <mr.bernardobvb@gmail.com>
2026-09-11 14:49:45 -0300

    add the reading files

--- 16_clockface/clockface.go
+++ 16_clockface/clockface.go
@@ -5,6 +5,15 @@ import (
 	"time"
 )
 
+const (
+	secondsInHalfClock = 30
+	secondsInClock     = 2 * secondsInHalfClock
+	minutesInHalfClock = 30
+	minutesInClock     = 2 * minutesInHalfClock
+	hoursInHalfClock   = 6
+	hoursInClock       = 2 * hoursInHalfClock
+)
+
 // A Point represents a two dimensional Cartesian coordinate.
 type Point struct {
 	X float64
@@ -12,24 +21,31 @@ type Point struct {
 }
 
 func secondsInRadians(t time.Time) float64 {
-	return (math.Pi / (30 / float64(t.Second())))
+	return (math.Pi / (secondsInHalfClock / float64(t.Second())))
 }
 
 func secondHandPoint(t time.Time) Point {
-	angle := secondsInRadians(t)
-	x := math.Sin(angle)
-	y := math.Cos(angle)
-
-	return Point{x, y}
+	return angleToPoint(secondsInRadians(t))
 }
 
 func minutesInRadians(t time.Time) float64 {
-	return (secondsInRadians(t) / 60) +
-		(math.Pi / (30 / float64(t.Minute())))
+	return (secondsInRadians(t) / minutesInClock) +
+		(math.Pi / (minutesInHalfClock / float64(t.Minute())))
 }
 
 func minuteHandPoint(t time.Time) Point {
-	angle := minutesInRadians(t)
+	return angleToPoint(minutesInRadians(t))
+}
+
+func hoursInRadians(t time.Time) float64 {
+	return (minutesInRadians(t) / hoursInClock) + (math.Pi / (hoursInHalfClock / float64(t.Hour()%hoursInClock)))
+}
+
+func hourHandPoint(t time.Time) Point {
+	return angleToPoint(hoursInRadians(t))
+}
+
+func angleToPoint(angle float64) Point {
 	x := math.Sin(angle)
 	y := math.Cos(angle)
 
---
+++ 17_Reading_files/blogposts/go.mod
@@ -0,0 +1,3 @@
+module github.com/bernardoer/blogposts
+
+go 1.26.6
