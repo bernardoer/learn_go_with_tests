f1f779cf1a6f414d671aa4ca291bb31e78c2f9be
bernardoer <mr.bernardobvb@gmail.com>
2026-09-15 08:27:16 -0300

    begin of generics

---
+++ 19_generics/generics_test.go
@@ -0,0 +1,29 @@
+package generics_test
+
+import "testing"
+
+func TestAssertFunctinos(t *testing.T) {
+	t.Run("asserting on integers", func(t *testing.T) {
+		AssertEqual(t, 1, 1)
+		AssertNotEqual(t, 1, 2)
+	})
+
+	t.Run("asserting on strings", func(t *testing.T) {
+		AssertEqual(t, "hello", "hello")
+		AssertNotEqual(t, "hello", "Grace")
+	})
+}
+
+func AssertEqual(t *testing.T, got, want int) {
+	t.Helper()
+	if got != want {
+		t.Errorf("got %d, want %d", got, want)
+	}
+}
+
+func AssertNotEqual(t *testing.T, got, want int) {
+	t.Helper()
+	if got == want {
+		t.Errorf("didn't want %d", got)
+	}
+}
