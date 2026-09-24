fa936256ae001cb54c8017011610488ddb469504
bernardoer <mr.bernardobvb@gmail.com>
2026-09-24 14:40:49 -0300

    add docker

--- 22_scaling_acceptance_tests/cmd/httpserver/greeter_server_test.go
+++ 22_scaling_acceptance_tests/cmd/httpserver/greeter_server_test.go
@@ -1,12 +1,39 @@
 package main_test
 
 import (
+	"context"
 	"testing"
 
+	go_specs_greet "github.com/bernardoer/learn_go_with_tests/go-specs-greet"
 	"github.com/bernardoer/learn_go_with_tests/go-specs-greet/specifications"
+	"github.com/quii/go-graceful-shutdown/assert"
+	"github.com/testcontainers/testcontainers-go"
+	"github.com/testcontainers/testcontainers-go/wait"
 )
 
 func TestGreeterServer(t *testing.T) {
-	driver := go_spects_greet.Driver{BaseURL: "http://localhost:8080"}
+	ctx := context.Background()
+
+	req := testcontainers.ContainerRequest{
+		FromDockerfile: testcontainers.FromDockerfile{
+			Context:    "../../.",
+			Dockerfile: "./cmd/httpserver/Dockerfile",
+			// set false to less spam, helpful when having troubles
+			PrintBuildLog: true,
+		},
+		ExposedPorts: []string{"8080:8080"},
+		WaitingFor:   wait.ForHTTP("/").WithPort("8080"),
+	}
+	container, err := testcontainers.GenericContainer(ctx, testcontainers.GenericContainerRequest{
+		ContainerRequest: req,
+		Started:          true,
+	})
+	assert.NoError(t, err)
+	t.Cleanup(func() {
+		assert.NoError(t, container.Terminate(ctx))
+	})
+
+	driver := go_specs_greet.Driver{BaseURL: "http://localhost:8080"}
 	specifications.GreetSpecification(t, driver)
+
 }
--- go.mod
+++ go.mod
@@ -1,5 +1,5 @@
 module example.com/hello
 
-go 1.26.6
+go 1.23
 
 require github.com/quii/go-graceful-shutdown v0.6.0 // indirect
