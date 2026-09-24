fe3b208f3a18e25f0ed5b0a5a8b3f27eae88f32c
bernardoer <mr.bernardobvb@gmail.com>
2026-09-24 17:15:25 -0300

    gospecsgreet

---
+++ 22_scaling_acceptance_tests/greet_test.go
@@ -0,0 +1,15 @@
+package go_specs_greet_test
+
+import (
+	"testing"
+
+	gospecsgreet "github.com/bernardoer/learn_go_with_tests/"
+	"github.com/bernardoer/learn_go_with_tests/go-specs-greet/specifications"
+)
+
+func TestGreet(t *testing.T) {
+	specifications.GreetSpecification(
+		t,
+		specifications.GreetAdapter(gospecsgreet.Greet),
+	)
+}
---
+++ 22_scaling_acceptance_tests/specifications/adapters.go
@@ -0,0 +1,6 @@
+type GreetAdapter func(name string) string
+
+func (g GreetAdapter) Greet(name string) (string, error) {
+	return g(name), nil
+}
+
